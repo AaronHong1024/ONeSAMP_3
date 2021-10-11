bf4c1676e9b277c03c784733bd5c170f78506f7b
kingufl <kingdgp@ufl.edu>
2021-10-11 18:09:26 +0530

    Finishing up stat1

--- stat1.py
+++ stat1.py
@@ -1,13 +1,11 @@
+import math
+
 def stat1(data):
-  print(data[0][1][2:])
   
-  alleleA=[]
-  alleleB=[]
+  alleleA=[] # same dimensions as data but holds the first allele in a locus
+  alleleB=[] # same dimensions as data but holds the second allele in a locus
   
-  print(len(data))
-  print(len(data[0]))
-  
-  for i in range(len(data)):
+  for i in range(len(data)): # fills up alleleA and alleleB
       temA=[]
       temB=[]
       for j in range(1,len(data[i])):
@@ -17,10 +15,15 @@ def stat1(data):
       alleleA.append(temA)
       alleleB.append(temB)
   
-  allcnt=[]
-  for j in range(len(alleleA[0])):
+  allcnt=[] #a 1D array, each element is a dictionary of alleles with frequency counts per loci
+  homoloci=[] # maintains frequency counts of homologous alleles per loci
+  for j in range(len(alleleA[0])): # fills up allcnt
       newdic={}
+      temp=0
       for i in range(len(alleleA)):
+          if(alleleA[i][j]==alleleB[i][j]):
+              temp+=1
+
           if(alleleA[i][j] in newdic):
               newdic[alleleA[i][j]]+=1
           else:
@@ -31,31 +34,51 @@ def stat1(data):
           else:
               newdic[alleleB[i][j]]=1
       
+      homoloci.append(temp/len(data)) 
       allcnt.append(newdic)
       
   print(allcnt[0])
-  di=[]
-  totspots=len(data)*2
-  print(totspots)
-  for i in range(len(allcnt)):
+  di=[] # a 1D array that holds the departures of each loci from Hardy-Weinberg equilibrium
+  totspots=len(data)*2 #total number of alleles per locus. used in computing allele freq per locus
+
+  for i in range(len(allcnt)): #fills up di 
       vals=[]
       for key,value in allcnt[i].items():
           vals.append(value/totspots)
-      
-      if(len(vals)>1):
-          di.append(vals[0]*vals[0]+vals[1]*vals[1]+2*vals[0]*vals[1]-1)
-      else:
-          di.append(0)
+      di.append(homoloci[i]*homoloci[i] - vals[0]*vals[0])
       
   hits=0
   
+  running_sum=0
+  
   for i in range(len(alleleA)):
       for j in range(len(alleleB)):
-          for k in range(len(alleleA[0])):
-              if(alleleA[i][j]==alleleB[i][j]):
-                  hits+=1
+          keysi=[]
+          keysj=[]
+
+          for key,value in allcnt[i].items():
+              keysi.append(key)
               
+          for key,value in allcnt[j].items():
+              keysj.append(key)
+          
+          if(len(keysj)<2):
+              continue
+          
+          alA=keysi[0]
+          alB=keysj[1]
+          
+          hits=0
+          for k in range(len(alleleA[0])):
+              if( (alleleA[i][k]==keysi or alleleB[i][k]==keysi) and (alleleA[j][k]==keysj or alleleB[j][k]==keysj) ):
+                  hits+=1
+          ai=allcnt[i][alA]/totspots
+          bj=allcnt[j][alB]/totspots
+          x=(hits/len(data) - ai*bj)/((ai*(1-ai)+di[i])*(bj*(1-bj)+di[j]))
+          running_sum+=math.sqrt(abs(x))
   
-  print(hits)
+  numloci=len(alleleA[0])
+  print(2*running_sum/(numloci*(numloci-1)))
+  #return 2*running_sum/(numloci*(numloci-1)) 
   
   
