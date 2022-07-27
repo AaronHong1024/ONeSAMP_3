adfee6ad9c68a4ce2390d12bd901e94f1596efaf
IshaYoo <73917514+IshaYoo@users.noreply.github.com>
2022-07-27 17:13:32 -0400

    Update oneSampBash-MR.sh

--- scripts/oneSampBash-MR.sh
+++ scripts/oneSampBash-MR.sh
@@ -17,12 +17,3 @@ echo "Running plot script on a single CPU core"
 #Running w default mutation rate
 python3 /blue/boucher/ishayooseph/oneSampTest/main.py --o /blue/boucher/ishayooseph/genePop500Ix500L > defaultMutationRate.txt
 
-#Running w 0.0000012  mutation rate
-python3 /blue/boucher/ishayooseph/oneSampTest/main.py --r 0.0000012 --o /blue/boucher/ishayooseph/genePop500Ix500L > 0000012MutationRate.txt
-
-#Running w 0.00012 mutation rate
-python3 /blue/boucher/ishayooseph/oneSampTest/main.py --r 0.00012  --o /blue/boucher/ishayooseph/genePop500Ix500L > 00012MutationRate.txt
-
-#Running w 0.012 mutation rate
-python3 /blue/boucher/ishayooseph/oneSampTest/main.py --r 0.012 --o /blue/boucher/ishayooseph/genePop500Ix500L > 012MutationRate.txt
-date
